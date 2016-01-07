410ebce67701cd9c56a889e5602cceef1513dab7
martykan <t.martykan@gmail.com>
2016-01-07 19:28:28 +0100

    Background autorefresh

---
+++ app/src/main/java/cz/martykan/forecastie/AlarmReceiver.java
@@ -0,0 +1,116 @@
+package cz.martykan.forecastie;
+
+import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.preference.PreferenceManager;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Locale;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    Context context;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("Alarm", "Recurring alarm; requesting download service.");
+        this.context = context;
+        if (isNetworkAvailable()) {
+            new GetWeatherTask().execute();
+            new GetLongTermWeatherTask().execute();
+        }
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    public class GetWeatherTask extends AsyncTask<String, String, Void> {
+
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            String result = "";
+            try {
+                SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+                String language = Locale.getDefault().getLanguage();
+                if(language.equals("cs")) { language = "cz"; }
+                URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q=" + URLEncoder.encode(sp.getString("city", "London"), "UTF-8") + "&lang="+ language +"&appid=2de143494c0b295cca9337e1e96b00e0");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                BufferedReader r = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+                if(urlConnection.getResponseCode() == 200) {
+                    String line = null;
+                    while ((line = r.readLine()) != null) {
+                        result += line + "\n";
+                    }
+                }
+                else {
+                    // Connection problem
+                }
+            } catch (IOException e) {
+                // No connection
+            }
+            return null;
+        }
+
+        protected void onPostExecute(Void v) {
+
+        }
+    }
+
+    class GetLongTermWeatherTask extends AsyncTask<String, String, Void> {
+
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            String result = "";
+            try {
+                SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+                String language = Locale.getDefault().getLanguage();
+                if(language.equals("cs")) { language = "cz"; }
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast?q=" + URLEncoder.encode(sp.getString("city", "London"), "UTF-8") + "&lang="+ language +"&mode=json&appid=2de143494c0b295cca9337e1e96b00e0");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                BufferedReader r = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+                if(urlConnection.getResponseCode() == 200) {
+                    String line = null;
+                    while ((line = r.readLine()) != null) {
+                        result += line + "\n";
+                    }
+                }
+                else {
+                    // Connection problem
+                }
+            } catch (IOException e) {
+                // No connection
+            }
+            return null;
+        }
+
+        protected void onPostExecute(Void v) {
+
+        }
+    }
+}
