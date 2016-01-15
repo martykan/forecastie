05fce1ff0e3a230e2b1f55e7a54134ba0a4ff1cb
icasdri <icasdri@gmail.com>
2016-01-14 21:16:33 -0500

    Fixed mistake in date setting logic from previous commit

--- app/src/main/java/cz/martykan/forecastie/Weather.java
+++ app/src/main/java/cz/martykan/forecastie/Weather.java
@@ -91,12 +91,18 @@ public class Weather {
     }
 
     public void setDate(String dateString) {
-        SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
         try {
-            setDate(inputFormat.parse(dateString));
+            setDate(new Date(Long.parseLong(dateString) * 1000));
         }
-        catch (ParseException e2) {
-            e2.printStackTrace();
+        catch (Exception e) {
+            SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
+            try {
+                setDate(inputFormat.parse(dateString));
+            }
+            catch (ParseException e2) {
+                setDate(new Date()); // make the error somewhat obvious
+                e2.printStackTrace();
+            }
         }
     }
 
