fd1750232f6f5a2cd51513b3f30050670a6c3ee7
Vincent Mazoyer <vmazoyer@users.noreply.github.com>
2016-04-13 21:10:14 +0200

    Display sunrise and sunset

--- app/src/main/java/cz/martykan/forecastie/Weather.java
+++ app/src/main/java/cz/martykan/forecastie/Weather.java
@@ -67,6 +67,8 @@ public class Weather {
     private String rain;
     private String id;
     private String icon;
+    private Date sunrise;
+    private Date sunset;
 
     public String getCity() {
         return city;
@@ -144,6 +146,54 @@ public class Weather {
 
     public void setHumidity(String humidity) {
         this.humidity = humidity;
+    }
+
+    public Date getSunrise(){
+        return this.sunrise;
+    }
+
+    public void setSunrise(String dateString) {
+        try {
+            setSunrise(new Date(Long.parseLong(dateString) * 1000));
+        }
+        catch (Exception e) {
+            SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
+            try {
+                setSunrise(inputFormat.parse(dateString));
+            }
+            catch (ParseException e2) {
+                setSunrise(new Date()); // make the error somewhat obvious
+                e2.printStackTrace();
+            }
+        }
+    }
+
+    public void setSunrise(Date date) {
+        this.sunrise = date;
+    }
+
+    public Date getSunset(){
+        return this.sunset;
+    }
+
+    public void setSunset(String dateString) {
+        try {
+            setSunset(new Date(Long.parseLong(dateString) * 1000));
+        }
+        catch (Exception e) {
+            SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
+            try {
+                setSunrise(inputFormat.parse(dateString));
+            }
+            catch (ParseException e2) {
+                setSunset(new Date()); // make the error somewhat obvious
+                e2.printStackTrace();
+            }
+        }
+    }
+
+    public void setSunset(Date date) {
+        this.sunset = date;
     }
 
     public String getIcon() {
