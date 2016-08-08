422f9c3c38ba99ae4a922eb3c45b79042955f2f4
SpiritCroc <spiritcroc@gmail.com>
2016-08-08 09:33:00 +0200

    Fix different colors per day sometimes starting in the middle of the day
    Closes martykan/forecastie#106

--- app/src/main/java/cz/martykan/forecastie/models/Weather.java
+++ app/src/main/java/cz/martykan/forecastie/models/Weather.java
@@ -237,14 +237,14 @@ public class Weather {
         initial.set(Calendar.MILLISECOND, 0);
         initial.set(Calendar.SECOND, 0);
         initial.set(Calendar.MINUTE, 0);
-        initial.set(Calendar.HOUR, 0);
+        initial.set(Calendar.HOUR_OF_DAY, 0);
 
         Calendar me = Calendar.getInstance();
         me.setTime(this.date);
         me.set(Calendar.MILLISECOND, 0);
         me.set(Calendar.SECOND, 0);
         me.set(Calendar.MINUTE, 0);
-        me.set(Calendar.HOUR, 0);
+        me.set(Calendar.HOUR_OF_DAY, 0);
 
         return Math.round((me.getTimeInMillis() - initial.getTimeInMillis()) / 86400000.0);
     }
