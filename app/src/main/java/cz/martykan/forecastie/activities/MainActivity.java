75c6b7af14fd4be4e686f1781d2073f250bb77e2
Juan Garcia <juanitobananas@gmail.com>
2016-08-11 20:45:02 +0200

    Request location only once
    RemoveUpdates from LocationManager once the onLocationChanged
    method is called (a location could be determined).

--- app/src/main/java/cz/martykan/forecastie/activities/MainActivity.java
+++ app/src/main/java/cz/martykan/forecastie/activities/MainActivity.java
@@ -744,6 +744,11 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     @Override
     public void onLocationChanged(Location location) {
         progressDialog.hide();
+        try {
+            locationManager.removeUpdates(this);
+        } catch (SecurityException e) {
+            Log.e("LocationManager", "Error while trying to stop listening for location updates. This is probably a permissions issue", e);
+        }
         Log.i("LOCATION (" + location.getProvider().toUpperCase() + ")", location.getLatitude() + ", " + location.getLongitude());
         double latitude = location.getLatitude();
         double longitude = location.getLongitude();
