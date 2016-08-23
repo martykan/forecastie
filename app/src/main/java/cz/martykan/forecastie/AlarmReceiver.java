29ef1432eb34cad164b1203a713d4caf79190ec7
Juan Garcia <juanitobananas@gmail.com>
2016-08-23 22:51:42 +0200

    Determine location on connectivity changes. It is probable that a connectivity change indicates a location change.

--- app/src/main/java/cz/martykan/forecastie/AlarmReceiver.java
+++ app/src/main/java/cz/martykan/forecastie/AlarmReceiver.java
@@ -47,10 +47,11 @@ public class AlarmReceiver extends BroadcastReceiver {
                 getWeather();
             }
         } else if (ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())) {
-            // Get weather if last attempt failed
+            // Get weather if last attempt failed or if 'update location in background' is activated
             SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
             String interval = sp.getString("refreshInterval", "1");
-            if (!interval.equals("0") && sp.getBoolean("backgroundRefreshFailed", false)) {
+            if (!interval.equals("0") &&
+                    (sp.getBoolean("backgroundRefreshFailed", false) || isUpdateLocation())) {
                 getWeather();
             }
         } else {
