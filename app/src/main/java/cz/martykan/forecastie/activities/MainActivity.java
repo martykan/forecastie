b295eda49f3e8f2cf0407e34bb9be63d60b26b1a
Tomáš Martykán <t.martykan@gmail.com>
2018-11-23 16:24:14 +0100

    Fix issue with UVI loading

--- app/src/main/java/cz/martykan/forecastie/activities/MainActivity.java
+++ app/src/main/java/cz/martykan/forecastie/activities/MainActivity.java
@@ -223,6 +223,9 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         if (!lastUVIToday.isEmpty()) {
             double latitude = todayWeather.getLat();
             double longitude = todayWeather.getLon();
+            if(latitude == 0 && longitude == 0) {
+                return;
+            }
             new TodayUVITask(this, this, progressDialog).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,"coords", Double.toString(latitude), Double.toString(longitude));
         }
     }
@@ -531,7 +534,6 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     private void updateUVIndexUI() {
         try {
             if (todayWeather.getCountry().isEmpty()) {
-
                 return;
             }
         } catch (Exception e) {
