26a57f35fcb5a487b5a8eb64e129217c00d6211c
destinyfucker <tom1fei@163.com>
2019-11-20 20:56:23 +0800

    have a check for LocationManager.NETWORK_PROVIDER to prevent IllegalArgumentException crashes

--- app/src/main/java/cz/martykan/forecastie/activities/SettingsActivity.java
+++ app/src/main/java/cz/martykan/forecastie/activities/SettingsActivity.java
@@ -160,9 +160,11 @@ public class SettingsActivity extends PreferenceActivity
         // Workaround for CM privacy guard. Register for location updates in order for it to ask us for permission
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         try {
-            DummyLocationListener dummyLocationListener = new DummyLocationListener();
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, dummyLocationListener);
-            locationManager.removeUpdates(dummyLocationListener);
+            if (locationManager.getAllProviders().contains(LocationManager.NETWORK_PROVIDER)) {
+                DummyLocationListener dummyLocationListener = new DummyLocationListener();
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, dummyLocationListener);
+                locationManager.removeUpdates(dummyLocationListener);
+            }
         } catch (SecurityException e) {
             // This will most probably not happen, as we just got granted the permission
         }
