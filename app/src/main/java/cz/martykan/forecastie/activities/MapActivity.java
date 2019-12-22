bf484367a75cf10a08c9208316083ce4db268153
SIMSEUNGMIN <ssm4591@naver.com>
2019-12-22 23:47:46 +0900

    add the Map for Clouds

--- app/src/main/java/cz/martykan/forecastie/activities/MapActivity.java
+++ app/src/main/java/cz/martykan/forecastie/activities/MapActivity.java
@@ -73,16 +73,20 @@ public class MapActivity extends BaseActivity {
 
     private void setMapState(int item) {
         switch (item) {
+            case R.id.map_clouds:
+                webView.loadUrl("javascript:map.removeLayer(rainLayer);map.removeLayer(windLayer);map.removeLayer(tempLayer);"
+                        + "map.addLayer(cloudsLayer);");
+                break;
             case R.id.map_rain:
-                webView.loadUrl("javascript:map.removeLayer(windLayer);map.removeLayer(tempLayer);"
+                webView.loadUrl("javascript:map.removeLayer(cloudsLayer);map.removeLayer(windLayer);map.removeLayer(tempLayer);"
                         + "map.addLayer(rainLayer);");
                 break;
             case R.id.map_wind:
-                webView.loadUrl("javascript:map.removeLayer(rainLayer);map.removeLayer(tempLayer);"
+                webView.loadUrl("javascript:map.removeLayer(cloudsLayer);map.removeLayer(rainLayer);map.removeLayer(tempLayer);"
                         + "map.addLayer(windLayer);");
                 break;
             case R.id.map_temperature:
-                webView.loadUrl("javascript:map.removeLayer(windLayer);map.removeLayer(rainLayer);"
+                webView.loadUrl("javascript:map.removeLayer(cloudsLayer);map.removeLayer(windLayer);map.removeLayer(rainLayer);"
                         + "map.addLayer(tempLayer);");
                 break;
             default:
